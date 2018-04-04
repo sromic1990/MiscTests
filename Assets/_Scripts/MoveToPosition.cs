e8b0c9b1d5928c6850b99bdf376682a959703275
Sourav Chatterjee <sourav.chatterjee@indianic.com>
2018-04-04 17:44:40 +0530

    - Added Scripts to move an object from point a to b in a fixed amount of time.

---
+++ Assets/_Scripts/MoveToPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPosition : MonoBehaviour 
+{
+    public Transform startPos, endPos;
+    public GameObject objectToMove;
+    public float timeToMove;
+    float t = 0;
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        t += Time.deltaTime / timeToMove;
+        objectToMove.transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+        //objectToMove.transform.position = Vector3.Lerp(objectToMove.transform.position, endPos.position, t);
+	}
+}
---
+++ Assets/_Scripts/MoveToPositionIenum.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPositionIenum : MonoBehaviour 
+{
+    public Transform startPos, endPos;
+    public GameObject objectToMove;
+    public float timeToMove;
+    float t = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine(MoveOverSeconds(objectToMove, startPos.position, endPos.position, timeToMove));
+	}
+	
+    public IEnumerator MoveOverSeconds(GameObject objectToMove, Vector3 startingPos, Vector3 end, float seconds)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < seconds)
+        {
+            objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        objectToMove.transform.position = end;
+    }
+}
