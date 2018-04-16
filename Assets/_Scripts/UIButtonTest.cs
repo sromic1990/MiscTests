0a87c65a9c6859e7200f5dcc55e8aa2436a739e1
Sourav Chatterjee <sourav.chatterjee@indianic.com>
2018-04-16 19:16:50 +0530

    - Button OnClick from script.

---
+++ Assets/_Scripts/UIButtonTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform), typeof(Button))]
+public class UIButtonTest : MonoBehaviour 
+{
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+        AddListenerToButton();
+	}
+
+    public void TestButton()
+    {
+        Debug.Log("This is a test");
+        DeleteListenerToButton();
+        StartCoroutine(ReassignToButton());
+    }
+
+    private IEnumerator ReassignToButton()
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Reassigned to button");
+        AddListenerToButton();
+    }
+
+    private void AddListenerToButton()
+    {
+        button.interactable = true;
+        button.onClick.AddListener(TestButton);
+    }
+
+    private void DeleteListenerToButton()
+    {
+        button.interactable = false;
+        button.onClick.RemoveListener(TestButton);
+    }
+}
